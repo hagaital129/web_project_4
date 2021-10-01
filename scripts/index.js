722f69f3842801638ecf0bcf8199f5d0e898b41e
Hagai Tal <hagaital129@gmail.com>
2021-10-01 13:56:47 +0300

    project 4

--- scripts/index.js
+++ scripts/index.js
@@ -8,28 +8,23 @@ let profileName = document.querySelector(".profile__name");
 let profileProfession = document.querySelector(".profile__profession");
 let profileEditButton = document.querySelector(".profile__edit-button");
 
-function openPopup(){
-    popup.classList.add("popup__is-opened");
-    popupInputName.value = profileName.textContent;
-    popupInputProfession.value = profileProfession.textContent;
+function openPopup() {
+  popup.classList.add("popup__is-opened");
+  popupInputName.value = profileName.textContent;
+  popupInputProfession.value = profileProfession.textContent;
 }
 
-function closePopup(){
-    popup.classList.remove("popup__is-opened");
+function closePopup() {
+  popup.classList.remove("popup__is-opened");
 }
 
-profileEditButton.addEventListener("click",openPopup);
+profileEditButton.addEventListener("click", openPopup);
 
 popupCloseButton.addEventListener("click", closePopup);
 
 profileFormWindow.addEventListener("submit", (event) => {
-    event.preventDefault();
-    profileName.textContent = popupInputName.value;
-    profileProfession.textContent = popupInputProfession.value;
-    closePopup();
-})
-
-
-
-
-
+  event.preventDefault();
+  profileName.textContent = popupInputName.value;
+  profileProfession.textContent = popupInputProfession.value;
+  closePopup();
+});
